b228db67ae77a4f0d50d599a961195f4f89ceb95
Stuart Carruthers <stu@ironiclensflare.com>
2018-06-04 15:23:51 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace graphdbtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
